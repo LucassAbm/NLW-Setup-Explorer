052097a349cca92410d1d250901afaee70df6410
LucassAbm <labmarinho@gmail.com>
2023-01-23 02:36:31 -0300

    adding some notes

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
-button.addEventListener("click", add)
+button.addEventListener("click", add) // Adiciona um 'ouvidor de eventos'. Nesse caso no atributo 'button'.
 form.addEventListener("change", save)
 
 function add() {
-  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5) // Formato de data
   const dayExists = nlwSetup.dayExists(today)
 
-  if (dayExists) {
+  if (dayExists) { // Se o dia já existir...
     alert("Dia já incluso 🔴")
-    return
+    return // Para a execução quando chegar aqui
   }
 
   alert("Adicionado com sucesso ✅")
@@ -19,9 +19,9 @@ function add() {
 }
 
 function save() {
-  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data)) // Transforma object em texto
 }
 
-const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {} // Transforma texto em object
 nlwSetup.setData(data)
 nlwSetup.load()
